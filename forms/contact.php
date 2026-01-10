756a18f246e4467d04aeae6101991f9637680028
SurfUX <tecnetsoftwaresolution@gmail.com>
2026-01-10 07:47:05 +0530

    update contact file

--- forms/contact.php
+++ forms/contact.php
@@ -1,15 +1,12 @@
 <?php
-// Show errors during testing (disable in production)
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Only allow POST
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header("Location: /#contact?error=invalid");
     exit;
 }
 
-// Sanitize input
 function clean($data) {
     return htmlspecialchars(strip_tags(trim($data)));
 }
@@ -24,24 +21,22 @@ if (!$name || !$email || !$subject || !$message) {
     exit;
 }
 
-// Email settings
-$to = "contact@surfux.com"; // CHANGE IF NEEDED
-$headers  = "From: $name <$email>\r\n";
+$to = "contact@surfux.com";
+
+$headers  = "From: SurfUX Website <contact@surfux.com>\r\n";
 $headers .= "Reply-To: $email\r\n";
 $headers .= "Content-Type: text/plain; charset=UTF-8";
 
-// Email body
-$body = "You have received a new message from SurfUX website\n\n";
-$body .= "Name: $name\n";
-$body .= "Email: $email\n";
-$body .= "Subject: $subject\n\n";
-$body .= "Message:\n$message\n";
+$body =
+"You have received a new message from SurfUX website\n\n".
+"Name: $name\n".
+"Email: $email\n".
+"Subject: $subject\n\n".
+"Message:\n$message\n";
 
-// Send email
 if (mail($to, $subject, $body, $headers)) {
     header("Location: /#contact?sent=1");
-    exit;
 } else {
     header("Location: /#contact?error=mail");
-    exit;
 }
+exit;
